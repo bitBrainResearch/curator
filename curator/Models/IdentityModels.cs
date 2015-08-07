a4e288e91df9c48170abe6d6439cd90af09d50e9
bitBrainResearch <bitbrainresearch@gmail.com>
2015-08-07 04:53:39 -0400

    testing adding stuff to user class

--- curator/Models/IdentityModels.cs
+++ curator/Models/IdentityModels.cs
@@ -9,6 +9,9 @@ namespace curator.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
+        public System.DateTime BirthDate { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -16,6 +19,8 @@ namespace curator.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
